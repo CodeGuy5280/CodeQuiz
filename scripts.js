b0eb2a9a4db5ec59d0f7e29c9649d5137d796cae
Alexander Leal <alex.j.leal@live.com>
2020-10-18 19:36:58 -0600

    Changes to html, css && js.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,72 @@
 var quiz = document.getElementbyId("quiz");
 var submit = document.getElementbyId("submit");
 var results = document.getElemtnbyId("results");
+// var timer = 
+// var timeRemaining
 
-//
+
+//var quiz will house questions 
+//var submit will push answer to app and progress to next question
+//^^ inner.html will clear the page and refresh
+//var results will house the result screen with score and user initials input  
+
+
+
+/*
+-Add homescreen for application before first question prompt
+-Add a start button
+-A timer begins to countdown when the button is clicked
+-A question is presented when the button is clicked
+-When an answer is selected (buttonPress) >>> 
+-Another question is presented once the first question is answered
+-If the question is answered INCORRECTLY, time is subtracted from the timer
+-All questions answered (5 total) OR timer reaches 0, End Game screen presented
+-When the game is over (Pass/Fail) user initials && score can be saved
+*/
+
+//check user input with the answer of the question
+// if (userChoice === true) {
+    
+// }else{
+
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 //function for next question
-function next() {
-    currentQuestion++;
-       if (currentQuestion > questions.length - 1) {
-        endGame();
-        return;
-    }
-    var quizContent = "<h2>" + questions[currentQuestion].title + "</h2>"
-    document.getElementById("quizBody").innerHTML = quizContent;
+// function next() {
+//     currentQuestion++;
+//        if (currentQuestion > questions.length - 1) {
+//         endGame();
+//         return;
+//     }
+//     var quizContent = "<h2>" + questions[currentQuestion].title + "</h2>"
+//     document.getElementById("quizBody").innerHTML = quizContent;
