562156d80d3f9b28b57a71f39831bdaab6f168e9
Alexander Leal <alex.j.leal@live.com>
2020-10-27 17:00:44 -0600

    hello

--- endPage.js
+++ endPage.js
@@ -1,3 +1,4 @@
+//Page for end screen/submission of initials
 const username = document.querySelector("#username")
 const saveScoreBtn = document.querySelector("#saveScoreBtn")
 const finalScore = document.querySelector("#finalScore")
